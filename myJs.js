2aa3d5c3b53d704741e539ff9912c11cc4988ac2
IFFX <Felix.proger@gmail.com>
2018-08-09 01:29:17 -0300

    Add files via upload

--- myJs.js
+++ myJs.js
@@ -5,7 +5,7 @@
 function btn()
 {
     document.getElementById("a1").innerHTML = "Paragraph changed!" +window.innerWidth;
-	 $("div").offset(100,100);
+	 $("div").offset(10,10);
 	 $("div").animate({left: '250px'});
     
 }
