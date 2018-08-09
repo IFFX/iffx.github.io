62242f567e902950f559eb83c574f045a0641c56
IFFX <Felix.proger@gmail.com>
2018-08-09 01:51:58 -0300

    Add files via upload

--- myJs.js
+++ myJs.js
@@ -1,10 +1,10 @@
 
 
 
-    document.getElementById("a1").innerHTML = "Paragraph casdasd";
+   // document.getElementById("a1").innerHTML = "Paragraph casdasd";
 function btn()
 {
-    document.getElementById("a1").innerHTML = "Paragraph changed!" +window.innerWidth;
+    document.getElementById("a1").innerHTML = document.getElementById("a1").innerHTML + "Paragraph changed!" +window.innerWidth;
 	 $("div").offset(10,10);
 	 $("div").animate({left: '25px'});
     
